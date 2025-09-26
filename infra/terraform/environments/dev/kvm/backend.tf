a5790a1b4faf460d61c548e3f9bee411da2d758b
Hemen Kapadia <hemen.kapadia@gmail.com>
2025-09-26 01:26:40 -0700

    feat(tf-kvm): Created per org (workspace) deployment wrapper script
    terraform-org-deploy.sh is a wrapper script to facilitate per
    org/workspace infra deployment. Org and workspace have 1:1 mapping.

--- infra/terraform/environments/dev/kvm/backend.tf
+++ infra/terraform/environments/dev/kvm/backend.tf
@@ -1,5 +1,6 @@
 terraform {
-  backend "local" {
-    path = "backend/tf_local_backend.tfstate"
-  }
+  # The backend "local" block is intentionally left empty here.
+  # Workspace will create per workspace (org) backend directory under terraform.tfstate.d directory
+  # As a sideeffect an empty backend directory will be created in this location
+  backend "local" {}
 }
