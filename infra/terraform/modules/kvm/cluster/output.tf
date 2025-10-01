1f8f2adc90e0904408d8f23ea5bfeb61f76ef760
Hemen Kapadia <hemen.kapadia@gmail.com>
2025-10-01 16:19:12 -0700

    fix(tf-kvm): Improvement to cluster nodes properties in the terraform output
    Removing aspects of to list from the terraform output and displaying the
    important information as string.

--- infra/terraform/modules/kvm/cluster/output.tf
+++ infra/terraform/modules/kvm/cluster/output.tf
@@ -4,8 +4,8 @@ output "cluster_nodes" {
     for idx, n in module.cluster_nodes : {
       node_name        = n.domain_name
       node_id          = n.domain_id
-      node_ip_address  = n.ip_addresses
-      node_mac_address = n.mac_addresses
+      node_ip_address  = n.ip_addresses[0][0]
+      node_mac_address = n.mac_addresses[0]
     }
   ]
 }
