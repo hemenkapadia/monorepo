631b6e52bc957f18c023e073b6cca34ba8fe0c18
Hemen Kapadia <hemen.kapadia@gmail.com>
2026-01-10 14:59:52 -0800

    feat(tf-kvm): Add bridge capabilities in kvm network module
    Support both NAT and Bridge Modes

--- infra/terraform/modules/kvm/network/variables.tf
+++ infra/terraform/modules/kvm/network/variables.tf
@@ -10,13 +10,24 @@ variable "network_domain" {
 
 variable "network_address_cidr" {
   type        = list(string)
-  description = "CIDR block for the libvirt NAT network"
+  description = "CIDR block(s) for the libvirt network (not used in bridge mode)"
+  default     = []
 }
 
 variable "network_mode" {
   type        = string
-  description = "Mode of the libvirt network"
+  description = "Mode of the libvirt network. Options: 'nat' (default) or 'bridge'"
   default     = "nat"
+  validation {
+    condition     = contains(["nat", "bridge"], var.network_mode)
+    error_message = "network_mode must be either 'nat' or 'bridge'"
+  }
+}
+
+variable "network_bridge" {
+  type        = string
+  description = "Bridge interface name (required when network_mode = 'bridge', e.g., 'br0')"
+  default     = null
 }
 
 variable "network_autostart" {
@@ -27,13 +38,13 @@ variable "network_autostart" {
 
 variable "network_dhcp_enabled" {
   type        = bool
-  description = "Enable DHCP for the libvirt network. Enabled by default"
+  description = "Enable DHCP for the libvirt network (not used in bridge mode). Enabled by default for NAT mode"
   default     = true
 }
 
 variable "network_dns_enabled" {
   type        = bool
-  description = "Enable DNS for the libvirt network. Enabled by default"
+  description = "Enable DNS for the libvirt network (not used in bridge mode). Enabled by default for NAT mode"
   default     = true
 }
 
@@ -68,5 +79,19 @@ List of DNS host entries.
 Each object should have:
   - ip        (string)
   - hostname  (string)
+EOT
+}
+
+variable "network_dnsmasq_options" {
+  type = list(object({
+    option_name  = string
+    option_value = string
+  }))
+  default     = []
+  description = <<EOT
+List of dnsmasq options to be passed to the libvirt network.
+Each object should have:
+  - option_name  (string, e.g. "dhcp-option")
+  - option_value (string, e.g. "6,8.8.8.8")
 EOT
 }
